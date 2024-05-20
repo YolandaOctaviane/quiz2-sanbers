cc3816ef784f36d554cba8d9c7f24c178f1b0dbe
YolandaOctaviane <yolandaoctavi@gmail.com>
2024-05-20 22:42:35 +0700

    Custom Command Create Account Test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,6 +11,14 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add('BuatBaru', (first, last, email, password, passwordco) => { 
+    cy.get('#firstname').type(first)
+    cy.get('#lastname').type(last)
+    cy.get('#email_address').type(email)
+    cy.get('#password').type(password)
+    cy.get('#password-confirmation').type(passwordco)
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+ })
 //
 //
 // -- This is a child command --
